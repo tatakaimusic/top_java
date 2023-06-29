dc681e770cf22f1576fe02440ab83fbe4b3875aa
Nikita <nikitamasslennikov@mail.ru>
2023-06-29 11:53:08 +0300

    HW3 added MealTestData and MealServiceTest

---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,51 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.model.Meal;
+
+import static ru.javawebinar.topjava.MealTestData.MEAL1_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest {
+    @Autowired
+    private MealService service;
+
+    @Test
+    public void get() {
+        Meal meal = service.get(MEAL1_ID, USER_ID);
+
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getBetweenInclusive() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void create() {
+    }
+}
